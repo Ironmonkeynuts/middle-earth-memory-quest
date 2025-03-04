e2b4e9d2ecbb7d78829bbdb90b230a253131449a
David Doyle-Owen <thinkimagine@outlook.com>
2025-03-04 16:45:35 +0000

    Add code to import functions and variables and load index to jest in game.jest.js

--- assets/js/tests/game.test.js
+++ assets/js/tests/game.test.js
@@ -3,3 +3,25 @@
 /**
  * @jest-environment jsdom
  */
+
+// Correct the import path for game.js
+const { test, expect, beforeEach } = require('@jest/globals');
+const { startGame, createCards, flipCard, checkMatch } = require('../game');
+
+jest.spyOn(window, 'alert').mockImplementation(() => {});
+
+// Load index.html in Jest
+beforeAll(() => {
+    let fs = require('fs');
+    let fileContents = fs.readFileSync('index.html', 'utf-8');
+    document.open();
+    document.write(fileContents);
+    document.close();
+    // document.body.innerHTML = `
+    //     <div id="game-board"></div>
+    //     <div id="score">Score: 0</div>
+    //     <input id="pairs" value="4" />
+    //     <button id="start-btn"></button>
+    // `;
+    // const startBtn = document.getElementById('start-btn');  
+});
